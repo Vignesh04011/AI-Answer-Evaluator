948b99c41e4341057dca6af05e3a02759dad4b60
Vignesh Vane <vigneshvane200@gmail.com>
2025-10-06 16:15:56 +0530

    version mismatch solved

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+export const FLASK_SERVER = "http://192.168.0.102:5000"; 
+// ⚠️ Replace 192.168.x.x with your local machine’s IP address running Flask
+// Use http://10.0.2.2:5000 if testing on Android Emulator
+// Use http://127.0.0.1:5000 if testing on iOS simulator
